5eb9469d7a41877c8a481177c1bc8c45355e1f3e
Yusheng <zhouyusheng@126.com>
2018-07-31 16:49:20 +0800

    期权单元测试

--- UnitTest_Option.m
+++ UnitTest_Option.m
@@ -14,19 +14,81 @@ path(path,genpath(stockDicPath));                 % added
 % 证券交易账户选项设置
 Options.InitCash = 1000000;
 Options.VolumeRatio = 0.25; % 成交量限制不得超过当日成交量的固定比例
-Options.MinAmountPerHand = 100;
-Options.MinCommission = 5; % 最小佣金
-Options.Commission = 0.0008; % 佣金
-Options.StampTax = 0.001; % 卖出时征收的印花税
-Options.Slippage = 0.00246; % 滑点
+Options.CommissionPerContract = 4; % 每张合约佣金
+Options.Slippage = 0; % 滑点
 Options.PartialDeal = 1; % 开启自动部分成交模式
-Options.Short = 1; % 允许对交易标的进行做空
 Options.DelayDays = 3; % 交易失败则最大延迟交易天数，超过则放弃交易
+Options.OptLastSettlementType = 'Close'; %在前结算价缺失的情况下采用收盘价代替 ‘Settle’
 
-
+%% 1.数据导入
 % this could take a while
 start_time = '2015-02-09';
 end_time = '2018-04-30';
 [DB flag] = LoadOptionData_Local(dataPath,start_time,end_time,Options);
+%获取代码
+fields = fieldnames(DB);
+optionFieldnames = fields(1:end-5);
+% 初始化期权资产
+Asset = InitOptionAsset(DB,Options);
+%% 2.落单
+% 测试落单限制
+DB.CurrentK = 29;
+Asset = OrderOption(DB,Asset,Signal{2}.Stock,Signal{2}.Volume,Data.Open(1),Signal{2}.Type,Options);
 
-%% 数据导入
+% 首日落单
+DB.CurrentK = 1;
+Signal{1}.Volume = 5;
+Signal{1}.Stock = optionFieldnames{1}(4:end);
+Signal{1}.Type = 'Today';
+Data = getfield(DB, optionFieldnames{1});
+Asset = OrderOption(DB,Asset,Signal{1}.Stock, Signal{1}.Volume,Data.Open(1),Signal{1}.Type,Options);
+Asset = OrderOption(DB,Asset,Signal{1}.Stock, -Signal{1}.Volume-1,Data.Open(2),'Next',Options);
+
+Signal{2}.Volume = -7;
+Signal{2}.Stock = optionFieldnames{2}(4:end);
+Signal{2}.Type = 'Today';
+Data = getfield(DB, optionFieldnames{2});
+Asset = OrderOption(DB,Asset,Signal{2}.Stock,Signal{2}.Volume,Data.Open(1),Signal{2}.Type,Options);
+Asset = OrderOption(DB,Asset,Signal{2}.Stock,-Signal{2}.Volume+1,Data.Open(2),'Next',Options);
+
+Signal{3}.Volume = -8;
+Signal{3}.Stock = optionFieldnames{3}(4:end);
+Signal{3}.Type = 'Today';
+Data = getfield(DB, optionFieldnames{3});
+Asset = OrderOption(DB,Asset,Signal{3}.Stock,Signal{3}.Volume,Data.Open(1),Signal{3}.Type,Options);
+Asset = OrderOption(DB,Asset,Signal{3}.Stock,Signal{2}.Volume,Data.Open(2),'Next',Options);
+
+
+%% 撮合与结算
+DB.CurrentK = 1;
+Asset = ClearingOption(Asset,DB,Options);
+%检查资金变化
+pos = cell2mat(Asset.Position(DB.CurrentK));
+allMargins = sum(cell2mat(Asset.Margins(DB.CurrentK)));
+allFee = sum(cell2mat(Asset.DealFee(DB.CurrentK)));
+price = cell2mat(Asset.DealPrice(DB.CurrentK))*10000;
+allCost = sum(-pos.*price);
+after = Asset.InitCash +allCost - allFee - allMargins;
+error = Asset.Cash(DB.CurrentK) - after;                                    %错误应该接近于0
+
+
+DB.CurrentK = 2;
+Asset = ClearingOption(Asset,DB,Options);
+%检查资金变化
+vol = cell2mat(Asset.DealVolume(DB.CurrentK));
+allMargins_before = sum(cell2mat(Asset.Margins(DB.CurrentK-1)));
+allMargins_after = sum(cell2mat(Asset.Margins(DB.CurrentK)));
+margins_change = allMargins_after - allMargins_before;
+allFee = sum(cell2mat(Asset.DealFee(DB.CurrentK)));
+price = cell2mat(Asset.DealPrice(DB.CurrentK))*10000;
+allCost = sum(-vol.*price);
+after = Asset.Cash(DB.CurrentK-1) +allCost - allFee - margins_change;
+error = Asset.Cash(DB.CurrentK) - after;                                   %错误应该接近于0
+
+
+
+
+
+
+
+
