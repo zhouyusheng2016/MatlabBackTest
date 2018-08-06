b178e20fd37a95bba59fc162f9a4adcef5d9c6de
Yusheng <zhouyusheng@126.com>
2018-08-06 13:50:56 +0800

    更新期权账户每日净值到Asset.GrossAssests

---
+++ Option/RecordOptionAssetValueAtBarClose.m
@@ -0,0 +1,20 @@
+function  Asset  = RecordOptionAssetValueAtBarClose(Asset,DB,I,Options)
+cashAsset = Asset.Cash(I);                                                  %现金资产
+position = Asset.Position{I};                                               %仓量
+Stock = Asset.Stock{I};                                                     %名称
+forzenMargin = Asset.Margins{I};                                            %保证金
+
+close = [];                                                                 %按照股票名称获取当天收盘价
+contractUnit = [];                                                          %期权合约乘数
+for i = 1:length(Stock)
+     Data = getfield(DB,code2structname(Stock{i}, 'O'));
+     close = [close Data.Close(I)];
+     contractUnit = [contractUnit Data.ContractUnit(I)];
+end
+optionAsset = sum(close.*contractUnit.*position);                           %期权资产
+
+optionMargins = sum(forzenMargin);                                          %期权保证金总和
+
+Asset.GrossAssets(I) = cashAsset+optionAsset+optionMargins;                               %期权账户总资产记录
+end
+
