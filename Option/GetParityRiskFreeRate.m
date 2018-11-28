20181fdf6f5483cf3d371d549c0eedee2c641bcf
Yusheng <zhouyusheng@126.com>
2018-11-28 09:42:46 +0800

    添加考虑margin的情况

--- Option/GetParityRiskFreeRate.m
+++ Option/GetParityRiskFreeRate.m
@@ -1,6 +1,17 @@
-function [ rf ] = GetParityRiskFreeRate(CallPrice, PutPrice, UnderlyingPrice, Strike, TimeUntilExpiration)
+function [ rf ] = GetParityRiskFreeRate(CallPrice, PutPrice, UnderlyingPrice, Strike, TimeUntilExpiration, varargin)
 % 由put call parity 计算出的期权隐含利率
-rf = (1/TimeUntilExpiration)*log(Strike/(PutPrice+UnderlyingPrice-CallPrice));
+
+%直接计算
+if isempty(varargin)
+    rf = (1/TimeUntilExpiration)*log((Strike)/(PutPrice+UnderlyingPrice-CallPrice));
+    return
+end
+
+% 考虑
+if strcmp(varargin{1},'Margin')&&~isempty(varargin)
+    rf = (1/TimeUntilExpiration)*log((Strike+varargin{2})/(PutPrice+UnderlyingPrice-CallPrice+varargin{2}));
+    return
+end
 
 end
 
