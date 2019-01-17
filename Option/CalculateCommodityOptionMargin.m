a5227c68bf6daa5f495dc90439648a09fa8a7fa0
Yusheng <zhouyusheng@126.com>
2019-01-17 09:48:05 +0800

    计算铜豆粕白糖的保证金

---
+++ Option/CalculateCommodityOptionMargin.m
@@ -0,0 +1,15 @@
+function [ margin ] = CalculateCommodityOptionMargin(price,underlying,Strike,contractInfo)
+% 豆粕、白糖、铜期权的交易保证金
+% 计算的是每单位保证金，合约保证金需要乘以 ！！！标的期货合约交易单位！！！
+if strcmp(contractInfo.type,'call')
+    otmValue = max(Strike - underlying,0);
+elseif strcmp(contractInfo.type,'put')
+    otmValue = max(underlying - Strike,0);
+else
+    error('CalculateMargin.m: Wrong option type c/p')
+end
+
+margin = price + max(underlying - otmValue/2, underlying/2);
+
+end
+
