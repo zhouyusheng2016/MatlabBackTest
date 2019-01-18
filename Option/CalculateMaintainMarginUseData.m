d14a30c424577c14a908b09ceb1f979bbb6de02c
Yusheng <zhouyusheng@126.com>
2019-01-18 15:42:38 +0800

    更正字段错误

--- Option/CalculateMaintainMarginUseData.m
+++ Option/CalculateMaintainMarginUseData.m
@@ -1,7 +1,7 @@
 function [ margin ] = CalculateMaintainMarginUseData(DB, Data,I,Options)
 Underlying = GetOptionUnderlyingStruct(DB, Data,Options);                   %合约标的struct
 
-underlyingSettle = GetOptionUnderlyingSettlePriceByType(Underlying,...
+underlyingSettle = GetUnderlyingSettlePriceByType(Underlying,...
     I, Options.SettlementType);                                      %合约标的前结算价
 
 settle = GetOptionContractSettlePriceByType( Data,...                 %合约前结算价
