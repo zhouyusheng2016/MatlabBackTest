410a0ae8b0cd304dc757a835a820275d7e96dbf2
Yusheng <zhouyusheng@126.com>
2019-01-24 14:25:51 +0800

    增加计算通过卖出put获取无风险利率的方式

--- Option/GetParityRiskFreeRate.m
+++ Option/GetParityRiskFreeRate.m
@@ -1,5 +1,7 @@
 function [ rf ] = GetParityRiskFreeRate(CallPrice, PutPrice, UnderlyingPrice, Strike, TimeUntilExpiration, varargin)
 % 由put call parity 计算出的期权隐含利率
+% varargin{1} -- 计算方式
+% varargin{2} -- 卖出合约的保证金
 
 %直接计算
 if isempty(varargin)
@@ -7,9 +9,15 @@ if isempty(varargin)
     return
 end
 
-% 考虑
+% 默认计算以卖出call可获得的rate
 if strcmp(varargin{1},'Margin')&&~isempty(varargin)
     rf = (1/TimeUntilExpiration)*log((Strike+varargin{2})/(PutPrice+UnderlyingPrice-CallPrice+varargin{2}));
+    return
+end
+% 计算以卖出put可获得的rate
+if strcmp(varargin{1},'sellPutMargin')&&~isempty(varargin)
+    rf = (1/TimeUntilExpiration)*log((-Strike+varargin{2})/...
+        (-PutPrice-UnderlyingPrice+CallPrice+varargin{2}));
     return
 end
 
